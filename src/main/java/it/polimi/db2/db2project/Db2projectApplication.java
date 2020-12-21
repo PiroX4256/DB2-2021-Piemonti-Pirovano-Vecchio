92f7a039dfaf6d02008814d5312c34f84c0892d4
Luca Pirovano <luca.pirovano.98@gmail.com>
2020-12-21 09:38:43 +0100

    first commit

---
+++ src/main/java/it/polimi/db2/db2project/Db2projectApplication.java
@@ -0,0 +1,13 @@
+package it.polimi.db2.db2project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Db2projectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Db2projectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/it/polimi/db2/db2project/Db2projectApplicationTests.java
@@ -0,0 +1,13 @@
+package it.polimi.db2.db2project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Db2projectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
