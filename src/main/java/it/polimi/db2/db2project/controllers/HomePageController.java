e228a601d0ff9d4671516259602742e3d37a6dff
Luca Vecchio <33781164+vechus@users.noreply.github.com>
2021-01-10 13:07:06 +0100

    chore: optimize imports

--- src/main/java/it/polimi/db2/db2project/controllers/HomePageController.java
+++ src/main/java/it/polimi/db2/db2project/controllers/HomePageController.java
@@ -4,6 +4,7 @@ import it.polimi.db2.db2project.entities.Questionnaire;
 import it.polimi.db2.db2project.services.QuestionnaireService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
