cd22a1f1db6e5c8b0a6f19fe48268e14f48c5bf4
Luca Vecchio <33781164+vechus@users.noreply.github.com>
2021-01-15 14:38:09 +0100

    feat: admin inspect questionnaire page init

--- src/main/resources/public/src/components/AdminInspectQuestionnaire.vue
+++ src/main/resources/public/src/components/AdminInspectQuestionnaire.vue
@@ -1,11 +1,53 @@
 <template>
-  <div class="vue-tempalte"></div>
-
+  <div class="vue-tempalte">
+    <b-table striped hover :items="questionnaires">
+      <template #cell(productImage)="data">
+        <img :src="data.value" class="img-responsive img-product img-little" alt="Image">
+      </template>
+    </b-table>
+  </div>
 </template>
 
 <script>
+import axios from "axios";
+import {mapGetters} from "vuex";
+
 export default {
-  name: "AdminInspectQuestionnaire"
+  name: "AdminInspectQuestionnaire",
+  data() {
+    return {
+      questionnaires: [],
+      fields: [
+        {
+          key: 'productName',
+          label: 'Product Name',
+          sortable: true
+        },
+        {
+          key: 'date',
+          sortable: true
+        },
+        {
+          key: 'productImage',
+          label: 'Product Image'
+        }
+      ]
+    }
+  },
+  computed: {
+    ...mapGetters('veztore', [
+      'getAdminBearer',
+    ])
+  },
+  async created() {
+    await axios.get(`${process.env.VUE_APP_API_ROOT}/questionnaire/getAllQuestionnaires`, {
+      headers: {
+        'Authorization': `Bearer ${this.getAdminBearer}`
+      }
+    }).then(res => {
+      this.questionnaires = res.data;
+    })
+  }
 }
 </script>
 
