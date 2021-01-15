e72a8a129b4eeb4f06d435b7c8129902bed6e326
Luca Vecchio <33781164+vechus@users.noreply.github.com>
2021-01-15 13:06:05 +0100

    feat: modals

--- src/main/resources/public/src/main.js
+++ src/main/resources/public/src/main.js
@@ -1,11 +1,15 @@
 import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
-import store from "./store"
+import store from "./store";
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue';
 
 import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 import '@/assets/css/main.css'
 
+Vue.use(BootstrapVue);
+Vue.use(IconsPlugin);
 Vue.config.productionTip = false
 
 new Vue({
