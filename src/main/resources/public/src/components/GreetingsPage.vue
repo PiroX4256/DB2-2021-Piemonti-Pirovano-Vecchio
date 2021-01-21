1d6fcdf3d2f81dc954a4eeb2006c95d44bc5b618
Luca Pirovano <luca.pirovano.98@gmail.com>
2021-01-21 11:32:19 +0100

    fix: greetings page

--- src/main/resources/public/src/components/GreetingsPage.vue
+++ src/main/resources/public/src/components/GreetingsPage.vue
@@ -3,9 +3,9 @@
   <h2>Thanks!</h2>
   <h5>Your response has been registered.</h5>
   <div>
-  <b-icon-check2-circle role="img" font-scale="4" class="mr-auto"></b-icon-check2-circle>
+  <b-icon-check2-circle role="img" font-scale="4" class="mr-auto" style="color: darkgreen"></b-icon-check2-circle>
   </div>
-  <router-link to="/" tag="button" class="btn btn-outline-primary">Go back</router-link>
+  <router-link to="/" tag="button" class="btn btn-outline-success">Go back</router-link>
 </div>
 </template>
 
