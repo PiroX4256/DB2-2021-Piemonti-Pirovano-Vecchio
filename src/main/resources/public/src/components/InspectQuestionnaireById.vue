82594b913f3eef490128bad070bd2ef4badf05dd
Luca Vecchio <33781164+vechus@users.noreply.github.com>
2021-01-21 12:30:51 +0100

    feat: admin inspection front-end

--- src/main/resources/public/src/components/InspectQuestionnaireById.vue
+++ src/main/resources/public/src/components/InspectQuestionnaireById.vue
@@ -1,5 +1,45 @@
 <template>
+  <!--  o List of users who submitted the questionnaire.
+        o List of users who cancelled the questionnaire.
+        o Questionnaire answers of each user. -->
   <div class="vue-tempalte">
+    <b-card-group deck>
+      <b-card header="Users who submitted the questionnaire">
+        <b-list-group>
+          <b-list-group-item v-for="(u_s, idx_s) in usersSubmitted" v-bind:key="idx_s">
+            {{ u_s }}
+          </b-list-group-item>
+        </b-list-group>
+      </b-card>
+    </b-card-group>
+
+    <b-card-group deck>
+      <b-card header="Users who cancelled the questionnaire">
+        <b-list-group>
+          <b-list-group-item v-for="(u_c, idx_c) in usersCancelled" v-bind:key="idx_c">
+            {{ u_c }}
+          </b-list-group-item>
+        </b-list-group>
+      </b-card>
+    </b-card-group>
+
+    <!-- List of questions with relative answers -->
+    <div class="card">
+      <ul class="list-group list-group-flush">
+        <li class="list-group-item"
+            v-for="(q, idx) in reviews" v-bind:key="idx"><h5>{{ q.marketingQuestion }}</h5>
+          <ul class="list-group">
+            <li class="list-group"
+                v-for="(a, idx) in q.marketingAnswerList" v-bind:key="idx">
+              <blockquote class="blockquote">
+                <p class="mb-0">{{ a.answer }}</p>
+                <footer class="blockquote-footer">User <cite title="Source Title">{{ a.user.username }}</cite></footer>
+              </blockquote>
+            </li>
+          </ul>
+        </li>
+      </ul>
+    </div>
 
   </div>
 </template>
@@ -15,7 +55,8 @@ export default {
       id: '',
       usersSubmitted: [],
       usersCancelled: [],
-
+      marketingQuestionList: [],
+      reviews: [],
     }
   },
   created() {
@@ -31,6 +72,16 @@ export default {
       this.usersSubmitted = res.data.submittedUsers;
       this.usersCancelled = res.data.cancelledUsers;
     });
+    axios.get(`${process.env.VUE_APP_API_ROOT}/admin/inspection`, {
+      params: {
+        questionnaireId: this.id
+      },
+      headers: {
+        'Authorization': `Bearer ${this.getAdminBearer}`
+      }
+    }).then(res => {
+      this.reviews = res.data;
+    })
   },
   computed: {
     ...mapGetters('veztore', [
